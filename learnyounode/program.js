e128a9a564aeb090a2aa82fed2b0e10c5b0cb6f0
barent <Nbc5004@gmail.com>
2016-02-03 20:07:04 +0000

    5 left in learnyounode

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -5,7 +5,9 @@
     //myFirstIO();
     //myFirstAsyncIO();
     //filteredLS();
-    makeItModular();
+    //makeItModular();
+    //httpClient();
+    httpCollect();
 })();
 
 function helloWorldNode(){
@@ -82,3 +84,29 @@ function makeItModular(){
     })
 }
 
+function httpClient(){
+    var http = require('http')
+
+    var url = process.argv[2]
+    
+        http.get(url, function (req) {
+        	req.setEncoding('utf8')
+        	req.on('data', console.log)
+        	req.on('error', console.error)
+    })
+}
+
+function httpCollect(){
+    var http = require('http')
+    var concat = require('concat-stream')
+    var url = process.argv[2]
+    
+    http.get(url, function (req) {
+      req.setEncoding('utf8')
+      var sink = concat(function(data){
+        console.log(data.split('').length)
+        console.log(data)
+      })
+      req.pipe(sink)
+    })
+}
